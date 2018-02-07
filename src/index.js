fe68a60a3897e54344347ba7657b78d792e18785
Andrew N <aninneman@gmail.com>
2018-02-07 12:57:48 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
+import './index.css';
+import './ui-toolkit/css/nm-cx/main.css';
+import store from './store';
+import App from './App';
+
+// Cannot render App directory because it does not have the store component
+const Root = () => {
+  return (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));

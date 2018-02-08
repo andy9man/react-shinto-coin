ddcdd4bbf75a925cfdb8791534f67398ae73bc95
Andrew N <aninneman@gmail.com>
2018-02-08 11:40:55 -0600

    Finalized LedgerDetails to handle if no ID is found

--- src/components/LedgerDetails.js
+++ src/components/LedgerDetails.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 const LedgerDetails = props => {
-    const {match: {params: {id}}}= props
     console.log("Display Ledger Details...");
-    console.log(props.ledger);
-    const item = props.ledger.find((item) => { return item.id==id})
+    const {match: {params: {id}}}= props
+    const {ledger} = props;
+    const item = ledger.length > 0 ? props.ledger.find((item) => { return item.id==id}) : undefined;
     console.log(item);
 
     console.log(props.ledger)
     return (
-        <div>
-            <p> Transaction <b># {id} </b></p>
+        <div className="margin-left-large">
+            {
+                item ?
+                    <div>
+                        <p> Transaction <b># {id} </b></p>
 
-            <p><b>{item.action} {item.amount} </b> ShintoCoin</p>
+                        <p><b>{item.action} {item.amount} </b> ShintoCoin</p>
+                    </div>
+                :
+                <h3>There are no Ledger Details Available.</h3>
+            }
         </div>
 
     );
